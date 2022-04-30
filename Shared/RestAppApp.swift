ae80eef0ce284c725f5a6a0ba3c119cb8c48e914
Rodrigo <rodrigo@rxdx.com.br>
2022-04-30 19:01:27 -0300

    Initial Commit

---
+++ Shared/RestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestAppApp.swift
+//  Shared
+//
+//  Created by Rodrigo Dumont on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
